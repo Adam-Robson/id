2f120450e80e1142015505b9e11cb9d7a2c6091f
Adam Robson <adamrayrobson@gmail.com>
2026-03-02 23:21:40 -0800

    add favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,24 @@ export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-       
+        <div className="flex items-center">
+          <Image
+            className="hidden dark:block"
+            src="/logodk.svg"
+            alt="Logo (dark)"
+            width={200}
+            height={200}
+            priority
+          />
+          <Image
+            className="block dark:hidden"
+            src="/logolt.svg"
+            alt="Logo (light)"
+            width={200}
+            height={200}
+            priority
+          />
+        </div>
       </main>
     </div>
   );
